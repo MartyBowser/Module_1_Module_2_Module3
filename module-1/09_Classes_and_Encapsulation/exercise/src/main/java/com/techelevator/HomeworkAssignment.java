8b0c9a6c81c2b066b82a92da2477e0d11ed75eb9
Martell Bowers <martellbowers@hotmail.com>
2022-09-22 14:23:19 -0400

    oh boy

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,56 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+    //state - variables
+    private int earnedMarks;
+    private int possibleMarks;
+    private String submitterName;
 
+    //Constructor
+    public HomeworkAssignment(int possibleMarks, String submitterName) {
+        this.possibleMarks = possibleMarks;
+        this.submitterName = submitterName;
+    }
+    //Methods
+
+    //getter
+    public int getEarnedMarks() {
+        return earnedMarks;
+    }
+
+    public int getPossibleMarks() {
+        return possibleMarks;
+    }
+
+    public String getSubmitterName() {
+        return submitterName;
+    }
+
+    public String getLetterGrade() {
+        String letterGrade = "";
+        double percent = ((double) earnedMarks / possibleMarks) * 100;
+        if (percent >= 90) {
+            letterGrade = "A";
+
+        } else if (percent >= 80) {
+            letterGrade = "B";
+        } else if (percent >= 70) {
+            letterGrade = "C";
+
+        } else if (percent >= 60) {
+
+            letterGrade = "D";
+
+        } else {
+            letterGrade = "F";
+        }
+        return letterGrade;
+    }
+
+//setters
+
+    public void setEarnedMarks(int earnedMarks) {
+        this.earnedMarks = earnedMarks;
+    }
 }
+
