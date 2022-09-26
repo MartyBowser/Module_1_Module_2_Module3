b473d0c0d3360e3adf7159b90965c34d01926fce
Martell Bowers <martellbowers@hotmail.com>
2022-09-26 12:04:36 -0400

    status

---
+++ module-1/11_Inheritance/lecture/src/main/java/com/techelevator/house.java
@@ -0,0 +1,25 @@
+package com.techelevator;
+
+public class house {
+
+    //state
+    private String banner;
+
+
+    //constructors
+    public house (String banner){
+        this.banner = banner;
+    }
+
+    //behavior - methods
+    //getters
+    public String getBanner(){
+        return banner;
+    }
+    public void setBanner() {
+        this.banner = banner;
+
+    }
+    //setters
+}
+
