1456382550cbf6ec4b052511cf54f1ee71d55eba
Martell Bowers <martellbowers@hotmail.com>
2022-09-28 14:51:29 -0400

    homework

---
+++ module-1/13_Managing_Inheritance/exercise/src/main/java/com/techelevator/SquareWall.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+public class SquareWall extends RectangleWall{
+    int sideLength;
+
+public SquareWall(String name, String color, int sideLength){
+    super(name, color, sideLength, sideLength);
+}
+
+public String toString(){
+    return getName()+ " ("+getLength()+"x"+getHeight()+") square";
+}
+
+
+}
+
