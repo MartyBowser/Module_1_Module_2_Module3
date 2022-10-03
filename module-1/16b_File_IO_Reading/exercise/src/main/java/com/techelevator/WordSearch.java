13addd44aa630e70d3a70f6fc7ced503eb658930
Martell Bowers <martellbowers@hotmail.com>
2022-10-03 17:11:02 -0400

    week 4 day 1

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,9 +1,37 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class WordSearch {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	}
+        int counter = 0;
+        Scanner userScan = new Scanner(System.in);
+        System.out.println("Please enter a file path");
+        String myPath = userScan.nextLine();
+        File myFile = new File(myPath);
 
+        System.out.println("Please enter a search word to check for");
+
+        String word = userScan.nextLine();
+        try (Scanner scan = new Scanner(myFile)) {
+
+            while (scan.hasNextLine()) {
+
+                String line = scan.nextLine();
+                counter++;
+
+                if (line.contains(word)) {
+                    System.out.println(counter + " " + line);
+                }
+            }
+        } catch (FileNotFoundException exception) {
+            System.out.println("We couldn't find the file you were looking for sorry");
+        }
+    }
 }
